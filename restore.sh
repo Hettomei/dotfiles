663d90c1ef2e6681eaab767fde78fbdb49cd7f6e
Hettomei <itsumo.sibyllin@gmail.com>
2013-02-22 21:02:24 +0100

    Restore script done

--- restore.sh
+++ restore.sh
@@ -2,6 +2,7 @@
 
 #get the dir where this script is launched
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 echo "create backup files into /backup" $DIR
 cp -v --backup=numbered \
   $HOME/.vimrc.after \
@@ -10,5 +11,10 @@ cp -v --backup=numbered \
   $HOME/.gitconfig \
   $HOME/.zshrc \
   $DIR/backup
-cd $DIR
 
+echo "restore files"
+cp -v $DIR/.vimrc.after $HOME/.vimrc.after
+cp -v $DIR/.vimrc.before $HOME/.vimrc.before
+cp -v $DIR/.gvimrc.after $HOME/.gvimrc.after
+cp -v $DIR/.gitconfig $HOME/.gitconfig
+cp -v $DIR/.zshrc $HOME/.zshrc
