e84dbb162fdd736c59211f8a8e7a87480ab9630f
Timothee Gauthier <t.gauthier@bemyboat.com>
2013-08-22 14:15:42 +0200

    Add new .ackrc file

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Copy file into" $DIR
 
-for file_name in 'vimrc' 'gvimrc' 'gitconfig' 'zshrc' 'irbrc'
+for file_name in 'vimrc' 'gvimrc' 'gitconfig' 'zshrc' 'irbrc' 'ackrc'
 do
   cp -v $HOME/.$file_name $DIR/$file_name
 done
