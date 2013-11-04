039cd540374faec0fa2f962e4a57f56b65ca73dc
Timothee Gauthier <t.gauthier@bemyboat.com>
2013-11-04 10:20:36 +0100

    reorder vimrc files

--- backup.sh
+++ backup.sh
@@ -4,9 +4,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Copy file into" $DIR
 
-for file_name in 'vimrc' 'gvimrc' 'gitconfig' 'zshrc' 'irbrc' 'ackrc'
+for file_name in 'gitconfig' 'zshrc' 'irbrc' 'ackrc'
 do
   cp -v $HOME/.$file_name $DIR/$file_name
+done
+
+for file_name in 'vimrc' 'gvimrc'
+do
+  cp -v $HOME/.vim/$file_name $DIR/vim/$file_name
 done
 
 rm $DIR/vim/plugin/settings/*
