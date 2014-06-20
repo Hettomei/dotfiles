4bc7b80ba5a43bb7924a6e336afaa3ffc9c732fd
Hettomei <itsumo.sibyllin@gmail.com>
2014-06-20 21:28:16 +0200

    Add NeoVim conf

--- copy_here_and_commit.sh
+++ copy_here_and_commit.sh
@@ -24,6 +24,18 @@ do
   cp -v -R $HOME/.vim/$file_name $DIR/vim/
 done
 
+
+#### NEOVIM ####
+rm -r $DIR/nvim
+mkdir $DIR/nvim
+
+cp -v $HOME/.nvim/nvimrc $DIR/nvim/nvimrc
+
+for file_name in 'after' 'my_snippets' 'dictionary'
+do
+  cp -v -R $HOME/.nvim/$file_name $DIR/nvim/
+done
+
 cd $DIR
 
 git add -p
---
+++ nvim/after/plugin/my_tabular_commands.vim
@@ -0,0 +1,1 @@
+AddTabularPattern arrow /=>
