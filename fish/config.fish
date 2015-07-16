64703740a8f55f20e70533a053154395054502ff
Timothée Gauthier <tgauthier@qosenergy.com>
2015-07-16 09:47:01 +0200

    Update fish

--- fish/config.fish
+++ fish/config.fish
@@ -10,4 +10,34 @@ alias v nvim
 alias g git
 alias bc "bc -l"
 
+# ag doesent use pager by default
+# I need to add a specific .agignorecustom because if I keep .agignore,
+# when I want to search with only .gitignore it is impossible because it combine .agignore with --path-to-agignore
+alias a 'ag --pager less --case-sensitive --path-to-agignore ".agignorecustom"'
+# find for file name. very usefull
+alias af 'ag --pager less -g'
+# Sometimes I want to use only .gitignore file
+alias aglarger 'ag --page less'
+# --unrestricted -> ALL fiels (ignore .gitignore and .agignore)
+alias aflarger 'ag  --pager less --unrestricted -g'
+
+alias f 'find . -name'
+alias rmDS 'find . -name ".DS_Store" -depth -exec rm {} \;'
+
+
+switch (uname)
+  case Linux
+    function qantumcommit
+      xdg-open http://gitlab.qosenergy.com/qosenergy/qantum/commit/$argv
+    end
+
+    set PATH $HOME/.linuxbrew/bin $PATH
+    set MANPATH $HOME/.linuxbrew/share/man $MANPATH
+    set INFOPATH $HOME/.linuxbrew/share/info $INFOPATH
+
+  case Mac
+  case '*'
+    echo You have an unknown uname (see .config/config.fish)
+end
+
 . (rbenv init -)
