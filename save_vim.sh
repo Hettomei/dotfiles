0870e556847dd161560002a28247ce2449bdda57
Timothée Gauthier <tgauthier@qosenergy.com>
2015-07-20 13:36:22 +0200

    Add a script to move my_computer_conf's vim inside it s good place

---
+++ save_vim.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#get the dir where this script is launched
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+for file_name in 'vimrc' 'gvimrc'
+do
+  cp -v $DIR/vim/$file_name $HOME/.vim/$file_name
+done
+
+for dir_name in 'my_snippets' 'dictionary'
+do
+  cp -v -R $DIR/vim/$dir_name $HOME/.vim/
+done
