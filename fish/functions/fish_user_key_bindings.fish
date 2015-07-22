00c871495fe68bcc7078031b7e8795bf070c4039
Timothée Gauthier <tgauthier@qosenergy.com>
2015-07-22 18:00:09 +0200

    More bind for fish

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,9 @@
 function fish_user_key_bindings
   fish_vi_key_bindings
+
+  bind -m insert o force-repaint
+  # bind -m insert o "commandline -a (echo ' \\\ ok'; echo '')[1] "
+
+  bind -M insert \ca beginning-of-line
+  bind -M insert \ce end-of-line
 end
