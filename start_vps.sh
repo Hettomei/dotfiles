8b155bff09e03019f958079622a5b17adb6ee3b7
Hettomei <itsumo.sibyllin@gmail.com>
2016-10-01 23:32:20 +0200

    vps java8

--- start_vps.sh
+++ start_vps.sh
@@ -18,7 +18,16 @@ g clone https://github.com/Hettomei/my_computer_conf.git
 cd my_computer_conf
 echo "run ./save_nvim.sh"
 
+# java 8
+cd
+add-apt-repository --yes ppa:webupd8team/java
+apt update
+apt install --yes oracle-java8-installer
+java -version
+
+
 # docker
+cd
 apt install --yes  apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' > /etc/apt/sources.list.d/docker.list
@@ -27,3 +36,4 @@ apt install --yes linux-image-extra-$(uname -r) linux-image-extra-virtual
 apt install --yes docker-engine
 service docker start
 docker run clojure
+
