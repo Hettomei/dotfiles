d1d6c1866130eab6345b8a6037c826172cd0b002
Hettomei <itsumo.sibyllin@gmail.com>
2016-10-01 22:44:26 +0200

    Quicly start a new vps with nvim + docker

---
+++ start_vps.sh
@@ -0,0 +1,29 @@
+set -ex
+
+sudo su
+cd
+
+## neovim
+apt update
+apt install --yes libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+alias g=git
+g clone https://github.com/neovim/neovim
+cd neovim
+make && make install
+alias v=nvim
+
+# conf nvim
+cd
+g clone https://github.com/Hettomei/my_computer_conf.git
+cd my_computer_conf
+echo "run ./save_nvim.sh"
+
+# docker
+apt install --yes  apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' > /etc/apt/sources.list.d/docker.list
+apt update
+apt install --yes linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt install --yes docker-engine
+service docker start
+docker run clojure
