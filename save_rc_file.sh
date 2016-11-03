434901eac801dba9596f0599bccfa645ed936abf
Timothée Gauthier <tgauthier@qosenergy.com>
2016-11-03 12:11:08 +0100

    Update save_rc

--- save_rc_file.sh
+++ save_rc_file.sh
@@ -13,5 +13,5 @@ echo touch $HOME/git-completion.bash or find it at
 echo https://github.com/git/git/blob/master/contrib/completion/git-completion.bash
 echo or
 echo https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-echo $HOME/.touch bashrc_not_in_git
+echo touch $HOME/.bashrc_not_in_git
 touch $HOME/.bashrc_not_in_git
