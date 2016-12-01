a6acba9e9e3224b8c28d03ca1aaf7daace4d73fc
Hettomei <itsumo.sibyllin@gmail.com>
2016-12-01 11:42:43 +0100

    update terminator, remove fish and vim copy

--- copy_here_and_commit.sh
+++ copy_here_and_commit.sh
@@ -16,20 +16,6 @@ do
   cp -v $HOME/$file_name $DIR/$file_name
 done
 
-#### VIM ####
-rm -r $DIR/vim
-mkdir $DIR/vim
-
-for file_name in 'vimrc' 'gvimrc' 'minimal_vimrc'
-do
-  cp -v $HOME/.vim/$file_name $DIR/vim/$file_name
-done
-
-for file_name in 'my_snippets' 'dictionary'
-do
-  cp -v -R $HOME/.vim/$file_name $DIR/vim/
-done
-
 #### NVIM ####
 rm -r $DIR/nvim
 mkdir $DIR/nvim
@@ -43,14 +29,6 @@ for file_name in 'my_snippets' 'dictionary'
 do
   cp -v -R $HOME/.config/nvim/$file_name $DIR/nvim/
 done
-
-#### FISH ####
-rm -r $DIR/fish
-mkdir $DIR/fish
-
-cp -v $HOME/.config/fish/config.fish $DIR/fish/config.fish
-cp -v $HOME/.config/fish/not_in_git.fish $DIR/fish/not_in_git.fish
-cp -v -R $HOME/.config/fish/functions $DIR/fish/
 
 ### TERMINATOR ###
 cp -v $HOME/.config/terminator/config $DIR/terminator_config
