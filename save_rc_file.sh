f911acdfe5c6327eccd0bfcf321f9155430dcf86
Timothée Gauthier <timothee@teezily.com>
2016-12-20 23:49:46 +0100

    update conf

--- save_rc_file.sh
+++ save_rc_file.sh
@@ -11,3 +11,7 @@ cp -v $DIR/tmux.conf $HOME/.tmux.conf
 
 echo touch $HOME/.bashrc_not_in_git
 touch $HOME/.bashrc_not_in_git
+
+echo 'to install tmux plugin :'
+echo "git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm"
+echo 'open tmux, then prefix + I'
