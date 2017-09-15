fd4bc51f2f4024aee8ccf84dca3e11a322bcd25d
Hettomei <itsumo.sibyllin@gmail.com>
2017-09-15 23:47:54 +0200

    vps.sh

--- vps.sh
+++ vps.sh
@@ -35,35 +35,35 @@ sudo su
 
 ## neovim
 cd
-apt update
-apt install --yes libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+sudo apt update
+sudo apt install --yes libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
 alias g=git
 g clone https://github.com/neovim/neovim
 cd neovim
-make && make install
+make && sudo make install
 alias v=nvim
 
 brew install ag
 brew install diff-so-fancy
 
 # conf nvim
-echo "run ./my_computer_conf/save_nvim.sh"
+# echo "run ./my_computer_conf/save_nvim.sh"
 
 # java 8
-cd
-add-apt-repository --yes ppa:webupd8team/java
-apt update
-apt install --yes oracle-java8-installer
-java -version
+# cd
+# add-apt-repository --yes ppa:webupd8team/java
+# apt update
+# apt install --yes oracle-java8-installer
+# java -version
 
 
 # docker
-cd
-apt install --yes  apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' > /etc/apt/sources.list.d/docker.list
-apt update
-apt install --yes linux-image-extra-$(uname -r) linux-image-extra-virtual
-apt install --yes docker-engine
-service docker start
-docker run clojure
+# cd
+# apt install --yes  apt-transport-https ca-certificates
+# sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+# echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' > /etc/apt/sources.list.d/docker.list
+# apt update
+# apt install --yes linux-image-extra-$(uname -r) linux-image-extra-virtual
+# apt install --yes docker-engine
+# service docker start
+# docker run clojure
