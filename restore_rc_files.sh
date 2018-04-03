e12cf353d1b39e0507460fccec47940282f4b8a2
Timothée Gauthier <timothee.gauthier.prestataire@wiztivi.com>
2018-04-03 10:33:41 +0200

    add profilee

--- restore_rc_files.sh
+++ restore_rc_files.sh
@@ -8,6 +8,7 @@ DIR="$CURRENT_DIR/${1:-default}"
 cp -v $DIR/irbrc $HOME/.irbrc
 cp -v $DIR/gemrc $HOME/.gemrc
 cp -v $DIR/bashrcc $HOME/.bashrcc
+cp -v $DIR/profilee $HOME/.profilee
 cp -v $DIR/gitconfig $HOME/.gitconfig
 
 echo touch $HOME/.bashrc_not_in_git
@@ -21,4 +22,5 @@ at the end of .bashrc
 you can copy/paste :
 
 echo 'source \$HOME/.bashrcc' >> $HOME/.bashrc
+echo 'source \$HOME/.profilee' >> $HOME/.profile
 ____HERE
