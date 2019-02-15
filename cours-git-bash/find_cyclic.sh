105252fcf2f3c1dc362ddd400d6088a51d4cd485
Timothée <tgauthier42@gmail.com>
2019-02-15 09:54:15 +0100

    git: better find_cyclic

--- cours-git-bash/find_cyclic.sh
+++ cours-git-bash/find_cyclic.sh
@@ -8,3 +8,19 @@ do
   git checkout HEAD~1 2> /dev/null
   echo '--------------'
 done
+
+
+# Output :
+
+# 813effb 2019-01-29 20:31:43 +0100 commit message 1
+# 17
+# --------------
+# 8b60365 2019-01-29 16:55:46 +0000 commit message 2
+# 17
+# --------------
+# 329d706 2019-01-29 16:51:36 +0000 commit message 3
+# 17
+# --------------
+# af8f4c4 2019-01-29 16:33:04 +0000 commit message 4
+# 87
+
