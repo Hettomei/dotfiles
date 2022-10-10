50f765e96009110831bc0c63241b3a334f9b033c
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2022-10-10 09:40:21 +0200

    Emacs conf for windows

---
+++ windows/emacs/README.md
@@ -0,0 +1,10 @@
+
+Where this file must goes in windows ?
+
+Must go there.
+
+C:\Users\tim\AppData\Roaming\.emacs.d\init.el
+
+If you already run emacs, it is possible that a file exist, you must delete it at : 
+
+C:\Users\tim\AppData\Roaming\.emacs
--- windows/emacs/init.el
+++ windows/emacs/init.el
@@ -13,33 +13,42 @@
 (setq package-enable-at-startup nil)
 
 (package-initialize)
-;; taken from https://github.com/emacs-evil/evil/tree/release-1.14
+
+;; How to add a new package
 ;; active this on the first run, not always
-;;(package-refresh-contents)
 
 ;; to make these lines available you have to
+;; alt-x package-refresh-contents
 ;; alt-x package-list-packages
 ;; C-s
 ;; find evil
 ;; install
 
-;; Evil auto install :
-;; Download Evil
+;; Evil auto install https://github.com/emacs-evil/evil/tree/release-1.14 :
 (unless (package-installed-p 'evil)
-  (package-install 'evil))
+  (progn
+    (package-refresh-contents)
+    (package-install 'evil)))
 
 ;; Enable Evil
 (require 'evil)
 (evil-mode 1)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages '(evil)))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+(setq make-backup-files nil) ; stop creating ~ files
+(global-display-line-numbers-mode)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
