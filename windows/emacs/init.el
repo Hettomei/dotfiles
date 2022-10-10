ead7f3364919163cf9a869ff886a00fa3a418159
Timothée Gauthier <timothee.gauthier-ext@pole-emploi.fr>
2022-10-10 10:56:25 +0200

    Use use-package for auto install :)

--- windows/emacs/init.el
+++ windows/emacs/init.el
@@ -1,10 +1,15 @@
-(setq inhibit-startup-screen t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Basic emacs configuration
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (set-language-environment "UTF-8")
 
-;; Put init.el into C-x b for convenience
-(find-file "c:/Users/tim/AppData/Roaming/.emacs.d/init.el")
+(setq inhibit-startup-screen t)
+(setq make-backup-files nil) ; stop creating ~ files
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Auto load 'use-package so it can autoload other packages
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'package)
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
@@ -14,37 +19,66 @@
 
 (package-initialize)
 
-;; How to add a new package
-;; active this on the first run, not always
-
-;; to make these lines available you have to
-;; alt-x package-refresh-contents
-;; alt-x package-list-packages
-;; C-s
-;; find evil
-;; install
-
-;; Evil auto install https://github.com/emacs-evil/evil/tree/release-1.14 :
-(unless (package-installed-p 'evil)
+(unless (package-installed-p 'use-package)
   (progn
     (package-refresh-contents)
-    (package-install 'evil)))
+    (package-install 'use-package)))
 
-(unless (package-installed-p 'undo-fu)
-  (progn
-    (package-refresh-contents)
-    (package-install 'undo-fu)))
+(eval-when-compile
+  (require 'use-package))
 
-;; Enable Evil
-(require 'evil)
-(evil-mode 1)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Install everything with use-package
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package magit
+  :ensure t)
+
+;; Vim style undo not needed for emacs 28
+(use-package undo-fu
+  :ensure t)
+
+(use-package evil
+  :ensure t
+  :requires undo-fu ;; should not be needed in emacs 28 but... need it on emacs 28 windows
+  :init (setq evil-ex-search-case (quote sensitive)
+	      evil-search-wrap nil
+	      evil-split-window-below t
+	      evil-vsplit-window-right t
+	      evil-cross-lines t
+	      evil-echo-state nil ; do not display "insert"
+	      ;; evil-search-module 'isearch ; Try it, I can't find the difference on internet
+	      evil-ex-substitute-global t ; automatic g in :s/aa/bb/g
+	      evil-undo-system 'undo-fu)
+  :config (evil-mode 1)) ; Or C-r will not work
+
+(use-package org
+  :ensure t)
+
+;;;;;;;;;;;;;;;;;;
+;; Other config
+;;;;;;;;;;;;;;;;;;
+
+(global-display-line-numbers-mode)
+
+;; Put init.el into C-x b for convenience
+(find-file "c:/Users/tim/AppData/Roaming/.emacs.d/init.el")
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+;;(global-set-key (kbd "C-c l") #'org-store-link)
+;;(global-set-key (kbd "C-c a") #'org-agenda)
+;;(global-set-key (kbd "C-c c") #'org-capture)
+
+;;;;;;;;;;;;;;;;;;;;;;;
+;; Automatically added
+;;;;;;;;;;;;;;;;;;;;;;;
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil)))
+ '(package-selected-packages '(use-package)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -52,22 +86,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-
-(setq make-backup-files nil) ; stop creating ~ files
-(global-display-line-numbers-mode)
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-
-(global-set-key (kbd "C-c l") #'org-store-link)
-(global-set-key (kbd "C-c a") #'org-agenda)
-(global-set-key (kbd "C-c c") #'org-capture)
-
-(setq evil-ex-search-case (quote sensitive)
-      evil-search-wrap nil
-      evil-split-window-below t
-      evil-vsplit-window-right t
-      evil-cross-lines t
-      evil-echo-state nil ; do not display "insert"
-      ;; evil-search-module 'isearch ; Try it, I can't find the difference on internet
-      evil-ex-substitute-global t ; automatic g in :s/aa/bb/g
-      evil-undo-system 'undo-fu) ; Or C-r will not work
