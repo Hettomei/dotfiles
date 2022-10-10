afc534597eb59c6ad88362c5191dd8e98e52c06d
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2022-10-10 09:29:25 +0200

    emacs windows auto install vim

--- windows/emacs/init.el
+++ windows/emacs/init.el
@@ -3,24 +3,43 @@
 (set-language-environment "UTF-8")
 
 ;; Put init.el into C-x b for convenience
-(find-file "c:/Users/Dev.PC-CONSERTO/AppData/Roaming/.emacs.d/init.el")
-(find-file "C:/Users/Dev.PC-CONSERTO/Documents/partage.org")
+(find-file "c:/Users/tim/AppData/Roaming/.emacs.d/init.el")
 
 (require 'package)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
 (setq package-enable-at-startup nil)
 
-(package-initialize) 
+(package-initialize)
+;; taken from https://github.com/emacs-evil/evil/tree/release-1.14
+;; active this on the first run, not always
+;;(package-refresh-contents)
 
 ;; to make these lines available you have to
 ;; alt-x package-list-packages
 ;; C-s
 ;; find evil
 ;; install
+
+;; Evil auto install :
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
 (require 'evil)
-(evil-mode t)
-
-
+(evil-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
