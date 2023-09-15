79baa721eadc4cd451d237db11a5653dfef46167
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2023-09-15 09:47:59 +0200

    redo polyinfra

--- default/tmux-env/polyinfra.sh
+++ default/tmux-env/polyinfra.sh
@@ -4,10 +4,17 @@
 # ./programmes/dotfiles/default/tmux-env/polyinfra.sh
 
 tmux new-session \; \
-  send-keys 'cd /c/Users/Dev.PC-CONSERTO/infracode' C-m \; \
+  send-keys 'cd /c/Users/Dev.PC-CONSERTO/infracode/poly-jenkins-job-pgd-mep' C-m \; \
   \
   new-window \; \
-  send-keys 'cd /c/Users/Dev.PC-CONSERTO/infracode' C-m \; \
+  send-keys 'cd /c/Users/Dev.PC-CONSERTO/infracode/poly-jenkins-job-pgd-mep' C-m \; \
+  \
+  new-window \; \
+  send-keys 'cd /c/Users/Dev.PC-CONSERTO/infracode/' C-m \; \
+  \
+  new-window \; \
+  send-keys 'cd /c/Users/Dev.PC-CONSERTO/programmes/apache-archiva-2.2.3' C-m \; \
+  send-keys './bin/archiva console' \; \
   \
   select-window -t 0 \; \
   select-pane -t 0 \;
