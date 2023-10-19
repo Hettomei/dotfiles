83d189e79792f8a245a192bfdb8edffd2dd9a310
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2023-10-19 21:10:26 +0000

    Add threejs

---
+++ default/tmux-env/threejs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Pour démarrer cette session : ne pas lancer tmux.
+# juste le bash, puis faire
+# ./programmes/dotfiles/default/tmux-env/formation.sh
+
+tmux new-session \; \
+  send-keys 'cd ~/programmes/codingame/javascript/webgl-play' C-m \; \
+  send-keys 'npm start' \; \
+  new-window \; \
+  send-keys 'cd ~/programmes/codingame/javascript/webgl-play' C-m \; \
+  new-window \; \
+  send-keys 'cd ~/programmes/codingame/javascript/webgl-play' C-m \; \
+  new-window \; \
+  select-window -t 0 \; \
+  select-pane -t 0 \;
